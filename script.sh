811a0c27cd47958081141da271615a1056d16ab6
Nareshkv5 <tonaresh.k@gmail.com>
2022-01-03 17:46:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+sudo apt-get update
+echo "Java 8 Installation"
+sudo apt install openjdk-8-jdk openjdk-8-jre -y
 echo "Installing Nexus"
-wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+sudo wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
 echo "Unzip the tar file"
-tar -xvf latest-unix.tar.gz
+sudo tar -xvf latest-unix.tar.gz
